2732576bc5e3c8bb9fdeba085dda1b041d5ef071
msDIZON <jasminedizon0728@gmail.com>
2025-03-29 11:32:32 +0800

    Create ObjectActivity.js

---
+++ ObjectActivity.js
@@ -0,0 +1,16 @@
+let car = { 
+    type: "Small", 
+    model: "Ferrari", 
+    color: "White" 
+    
+}; 
+
+// Check the type of the car, then console
+console.log(typeof car); 
+
+// Change the property of type to "Toyota", then console
+car.type = "Toyota"; 
+console.log("Updated Type Property:", car); 
+// Add a property which is wheel and has a value of 4, then console
+car.wheels = 4; 
+console.log("Added Property:", car);
