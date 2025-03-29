25e270cd314e4f149f0bea16c5a045631584100e
msDIZON <jasminedizon0728@gmail.com>
2025-03-29 18:34:42 +0800

    Update ArrayActivity.js

--- ArrayActivity.js
+++ ArrayActivity.js
@@ -1,1 +1,14 @@
-let 
+// Accessing an element inside the array by indexing 
+let favSingers = ["Soo Hyun", "Michael Jackson", "Bruno Mars"];
+// logging the first element in the array
+console.log(favSingers[0];
+
+// printing an element inside the array useing their index 
+let favNumbers = [20, 28, 1, 2];
+let mixedArr = [7, "Jasmine", true, "Dizon", 15];
+
+console.log(mixedArr[0];
+console.log(mixedArr[1];
+console.log(mixedArr[2];
+console.log(mixedArr[3];
+console.log(mixedArr[4];
